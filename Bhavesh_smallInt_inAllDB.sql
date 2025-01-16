67ba81bd1aefb729300ca1322a797d30ef32ef73
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-01-16 14:57:47 +0400

    Update Bhavesh_smallInt_inAllDB.sql

--- Bhavesh_smallInt_inAllDB.sql
+++ Bhavesh_smallInt_inAllDB.sql
@@ -1,19 +1,17 @@
-/* COLLATION ISSUE AT THE BOTTOM IS DUE*/
+/* Resolving Collation Issue */
 
-DECLARE @NL CHAR(2)
-SET @NL = CHAR(13) + CHAR(10)
+DECLARE @NL CHAR(2);
+SET @NL = CHAR(13) + CHAR(10);
 
-select 'select d.name, t.table_schema, t.table_name, c.column_name, c.data_type' +
-@NL + 'from sys.databases d' +
-@NL + 'join ' + d.name + '.information_schema.tables t' +
-@NL + ' on d.name = t.table_catalog' +
-@NL + 'join ' + d.name + '.information_schema.columns c' +
-@NL + ' on t.table_catalog = c.table_catalog' +
-@NL + ' and t.table_schema = c.table_schema' +
-@NL + ' and t.table_name = c.table_name' +
-@NL + 'where data_type = ''smallint''' + @NL + @NL
-from sys.databases d
-/*Msg 468, Level 16, State 9, Line 1
-Cannot resolve the collation conflict between "Latin1_General_CI_AS_KS_WS" and "SQL_Latin1_General_CP1_CI_AS" in the equal to operation.
-*/
-
+-- Generate the query dynamically for all databases
+SELECT 
+    'SELECT d.name, t.table_schema, t.table_name, c.column_name, c.data_type' +
+    @NL + 'FROM sys.databases d' +
+    @NL + 'JOIN ' + QUOTENAME(d.name) + '.information_schema.tables t' +
+    @NL + ' ON d.name COLLATE DATABASE_DEFAULT = t.table_catalog COLLATE DATABASE_DEFAULT' +
+    @NL + 'JOIN ' + QUOTENAME(d.name) + '.information_schema.columns c' +
+    @NL + ' ON t.table_catalog COLLATE DATABASE_DEFAULT = c.table_catalog COLLATE DATABASE_DEFAULT' +
+    @NL + ' AND t.table_schema = c.table_schema' +
+    @NL + ' AND t.table_name = c.table_name' +
+    @NL + 'WHERE c.data_type = ''smallint''' + @NL + @NL
+FROM sys.databases d;
